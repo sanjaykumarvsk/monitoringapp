83cb3a354c109cbc82fad22cc58547da19047e5b
Sanjay Kumar V <148225002+sanjaykumarvsk@users.noreply.github.com>
2025-04-15 01:04:50 +0530

    initial commit

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+	return (
+		<SettingSection icon={User} title={"Profile"}>
+			<div className='flex flex-col sm:flex-row items-center mb-6'>
+				<img
+					src='https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg'
+					alt='Profile'
+					className='rounded-full w-20 h-20 object-cover mr-4'
+				/>
+
+				<div>
+					<h3 className='text-lg font-semibold text-gray-100'>Sanjay Kumar</h3>
+					<p className='text-gray-400'>sanjaykumar@example.com</p>
+				</div>
+			</div>
+
+			<button className='bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto'>
+				Edit Profile
+			</button>
+		</SettingSection>
+	);
+};
+export default Profile;
